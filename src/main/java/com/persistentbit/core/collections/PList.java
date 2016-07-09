5138c7740c301b7e7d657ec8fc82dcf745237f0b
Peter Muys <peter@petermuys.com>
2016-07-09 17:01:40 +0200

    License info

--- src/main/java/com/persistentbit/core/collections/PList.java
+++ src/main/java/com/persistentbit/core/collections/PList.java
@@ -12,8 +12,17 @@ import java.util.*;
 import java.util.function.Function;
 
 /**
- * @author Peter Muys
- * @since 6/07/2016
+ *   Copyright(c) Peter Muys.
+ *   This code is base on the Persistent List created by Rich Hickey.
+ *   see copyright notice below.
+ *
+ *   Copyright (c) Rich Hickey. All rights reserved.
+ *   The use and distribution terms for this software are covered by the
+ *   Eclipse Public License 1.0 (http://opensource.org/licenses/eclipse-1.0.php)
+ *   which can be found in the file epl-v10.html at the root of this distribution.
+ *   By using this software in any fashion, you are agreeing to be bound by
+ * 	 the terms of this license.
+ *   You must not remove this notice, or any other, from this software.
  */
 public class PList<T> extends AbstractPSeq<T,PList<T>> implements Serializable{
 
--- src/main/java/com/persistentbit/core/collections/PMap.java
+++ src/main/java/com/persistentbit/core/collections/PMap.java
@@ -7,11 +7,18 @@ import com.persistentbit.core.Tuple2;
 import java.io.Serializable;
 import java.util.*;
 import java.util.function.Function;
-
 /**
- * User: petermuys
- * Date: 8/07/16
- * Time: 09:07
+ *   Copyright(c) Peter Muys.
+ *   This code is base on the PersistenHashMap created by Rich Hickey.
+ *   see copyright notice below.
+ *
+ *   Copyright (c) Rich Hickey. All rights reserved.
+ *   The use and distribution terms for this software are covered by the
+ *   Eclipse Public License 1.0 (http://opensource.org/licenses/eclipse-1.0.php)
+ *   which can be found in the file epl-v10.html at the root of this distribution.
+ *   By using this software in any fashion, you are agreeing to be bound by
+ * 	 the terms of this license.
+ *   You must not remove this notice, or any other, from this software.
  */
 public class PMap<K, V> extends PStreamDirect<Tuple2<K,V>,PMap<K,V>>{
     static final private Object sNullKey = new Object();
