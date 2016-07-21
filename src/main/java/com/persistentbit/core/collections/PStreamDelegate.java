b90b2a24ba6addd227fd48a927c16e29e27b25a4
Peter Muys <peter@petermuys.com>
2016-07-21 09:02:30 +0200

    Abstract PStream delegate

--- src/main/java/com/persistentbit/core/collections/PStreamDelegate.java
+++ src/main/java/com/persistentbit/core/collections/PStreamDelegate.java
@@ -15,306 +15,315 @@ import java.util.stream.Stream;
  * Date: 20/07/16
  * Time: 17:56
  */
-public class PStreamDelegate<T> implements PStream<T>{
+public abstract class PStreamDelegate<T> implements PStream<T>{
 
-    private final Supplier<PStream<T>> delegate;
+    
 
-
-    /**
-     * Create the PStream Delegated PStream
-     * @param delegate The supplier of the delegate.
-     */
-    public PStreamDelegate(Supplier<PStream<T>> delegate) {
-        this.delegate = delegate;
-    }
+    protected abstract PStream<T> getDelegate();
 
     @Override
     public PStream<T> lazy() {
-        return delegate.get().lazy();
+        return getDelegate().lazy();
     }
 
     @Override
     public boolean isInfinit() {
-        return delegate.get().isInfinit();
+        return getDelegate().isInfinit();
     }
 
     @Override
     public PStream<T> clear() {
-        return delegate.get().clear();
+        return getDelegate().clear();
     }
 
     @Override
     public PStream<T> limit(int count) {
-        return delegate.get().limit(count);
+        return getDelegate().limit(count);
     }
 
     @Override
     public PStream<T> dropLast() {
-        return delegate.get().dropLast();
+        return getDelegate().dropLast();
     }
 
     @Override
     public <R> PStream<R> map(Function<T, R> mapper) {
-        return delegate.get().map(mapper);
+        return getDelegate().map(mapper);
     }
 
     @Override
     public PStream<T> filter(Predicate<T> p) {
-        return delegate.get().filter(p);
+        return getDelegate().filter(p);
     }
 
     @Override
     public Optional<T> find(Predicate<T> p) {
-        return delegate.get().find(p);
+        return getDelegate().find(p);
     }
 
     @Override
     public <Z> PStream<Tuple2<Z, T>> zip(PStream<Z> zipStream) {
-        return delegate.get().zip(zipStream);
+        return getDelegate().zip(zipStream);
     }
 
     @Override
     public PStream<Tuple2<Integer, T>> zipWithIndex() {
-        return delegate.get().zipWithIndex();
+        return getDelegate().zipWithIndex();
     }
 
     @Override
     public Stream<T> stream() {
-        return delegate.get().stream();
+        return getDelegate().stream();
     }
 
     @Override
     public PStream<T> sorted(Comparator<? super T> comp) {
-        return delegate.get().sorted(comp);
+        return getDelegate().sorted(comp);
     }
 
     @Override
     public PStream<T> sorted() {
-        return delegate.get().sorted();
+        return getDelegate().sorted();
     }
 
     @Override
     public PStream<T> reversed() {
-        return delegate.get().reversed();
+        return getDelegate().reversed();
     }
 
     @Override
     public PStream<T> plusAll(Iterable<T> iter) {
-        return delegate.get().plusAll(iter);
+        return getDelegate().plusAll(iter);
     }
 
     @Override
     public PStream<T> flattenPlusAll(Iterable<Iterable<T>> iterIter) {
-        return delegate.get().flattenPlusAll(iterIter);
+        return getDelegate().flattenPlusAll(iterIter);
     }
 
     @Override
     public boolean contains(Object value) {
-        return delegate.get().contains(value);
+        return getDelegate().contains(value);
     }
 
     @Override
     public boolean containsAll(Iterable<?> iter) {
-        return delegate.get().containsAll(iter);
+        return getDelegate().containsAll(iter);
     }
 
     @Override
     public <K> PMap<K, PList<T>> groupBy(Function<T, K> keyGen) {
-        return delegate.get().groupBy(keyGen);
+        return getDelegate().groupBy(keyGen);
     }
 
     @Override
     public <K> PMap<K, T> groupByOneValue(Function<T, K> keyGen) {
-        return delegate.get().groupByOneValue(keyGen);
+        return getDelegate().groupByOneValue(keyGen);
     }
 
     @Override
     public <K, V> PMap<K, PList<V>> groupBy(Function<T, K> keyGen, Function<T, V> valGen) {
-        return delegate.get().groupBy(keyGen, valGen);
+        return getDelegate().groupBy(keyGen, valGen);
     }
 
     @Override
     public <K, V> PMap<K, V> groupByOneValue(Function<T, K> keyGen, Function<T, V> valGen) {
-        return delegate.get().groupByOneValue(keyGen, valGen);
+        return getDelegate().groupByOneValue(keyGen, valGen);
     }
 
     @Override
     public <K> POrderedMap<K, PList<T>> groupByOrdered(Function<T, K> keyGen) {
-        return delegate.get().groupByOrdered(keyGen);
+        return getDelegate().groupByOrdered(keyGen);
     }
 
     @Override
     public <K, V> POrderedMap<K, PList<V>> groupByOrdered(Function<T, K> keyGen, Function<T, V> valGen) {
-        return delegate.get().groupByOrdered(keyGen, valGen);
+        return getDelegate().groupByOrdered(keyGen, valGen);
     }
 
     @Override
     public PStream<T> plus(T value) {
-        return delegate.get().plus(value);
+        return getDelegate().plus(value);
     }
 
     @Override
     public T fold(T init, BinaryOperator<T> binOp) {
-        return delegate.get().fold(init,binOp);
+        return getDelegate().fold(init,binOp);
     }
 
     @Override
     public <X> X with(X init, BiFunction<X, T, X> binOp) {
-        return delegate.get().with(init,binOp);
+        return getDelegate().with(init,binOp);
     }
 
     @Override
     public Optional<T> headOpt() {
-        return delegate.get().headOpt();
+        return getDelegate().headOpt();
     }
 
     @Override
     public T head() {
-        return delegate.get().head();
+        return getDelegate().head();
     }
 
     @Override
     public <X> PStream<X> cast(Class<X> itemClass) {
-        return delegate.get().cast(itemClass);
+        return getDelegate().cast(itemClass);
     }
 
     @Override
     public Optional<T> lastOpt() {
-        return delegate.get().lastOpt();
+        return getDelegate().lastOpt();
     }
 
     @Override
     public Optional<T> beforeLastOpt() {
-        return delegate.get().beforeLastOpt();
+        return getDelegate().beforeLastOpt();
     }
 
     @Override
     public PStream<T> replaceFirst(T original, T newOne) {
-        return delegate.get().replaceFirst(original,newOne);
+        return getDelegate().replaceFirst(original,newOne);
     }
 
     @Override
     public PStream<T> tail() {
-        return delegate.get().tail();
+        return getDelegate().tail();
     }
 
     @Override
     public Optional<T> max(Comparator<T> comp) {
-        return delegate.get().max(comp);
+        return getDelegate().max(comp);
     }
 
     @Override
     public Optional<T> min(Comparator<T> comp) {
-        return delegate.get().min(comp);
+        return getDelegate().min(comp);
     }
 
     @Override
     public Optional<T> min() {
-        return delegate.get().min();
+        return getDelegate().min();
     }
 
     @Override
     public Optional<T> max() {
-        return delegate.get().max();
+        return getDelegate().max();
     }
 
     @Override
     public boolean isEmpty() {
-        return delegate.get().isEmpty();
+        return getDelegate().isEmpty();
     }
 
     @Override
     public int size() {
-        return delegate.get().size();
+        return getDelegate().size();
     }
 
     @Override
     public int count(Predicate<T> predicate) {
-        return delegate.get().count(predicate);
+        return getDelegate().count(predicate);
     }
 
     @Override
     public PStream<T> plusAll(T v1, T... rest) {
-        return delegate.get().plusAll(v1,rest);
+        return getDelegate().plusAll(v1,rest);
     }
 
     @Override
     public T[] toArray() {
-        return delegate.get().toArray();
+        return getDelegate().toArray();
     }
 
     @Override
     public <T1> T1[] toArray(T1[] a) {
-        return delegate.get().toArray(a);
+        return getDelegate().toArray(a);
     }
 
     @Override
     public PList<T> plist() {
-        return delegate.get().plist();
+        return getDelegate().plist();
     }
 
     @Override
     public PSet<T> pset() {
-        return delegate.get().pset();
+        return getDelegate().pset();
     }
 
     @Override
     public POrderedSet<T> porderedset() {
-        return delegate.get().porderedset();
+        return getDelegate().porderedset();
     }
 
     @Override
     public PStream<T> distinct() {
-        return delegate.get().distinct();
+        return getDelegate().distinct();
     }
 
     @Override
     public LList<T> llist() {
-        return delegate.get().llist();
+        return getDelegate().llist();
     }
 
     @Override
     public List<T> list() {
-        return delegate.get().list();
+        return getDelegate().list();
     }
 
     @Override
     public List<T> toList() {
-        return delegate.get().toList();
+        return getDelegate().toList();
     }
 
     @Override
     public Optional<T> join(BinaryOperator<T> joiner) {
-        return delegate.get().join(joiner);
+        return getDelegate().join(joiner);
     }
 
     @Override
     public <X> PStream<X> flatten() {
-        return delegate.get().flatten();
+        return getDelegate().flatten();
     }
 
     @Override
     public PStream<String> mapString() {
-        return delegate.get().mapString();
+        return getDelegate().mapString();
     }
 
     @Override
     public PStream<String> mapString(String nullValue) {
-        return delegate.get().mapString(nullValue);
+        return getDelegate().mapString(nullValue);
     }
 
     @Override
     public String toString(String sep) {
-        return delegate.get().toString(sep);
+        return getDelegate().toString(sep);
     }
 
     @Override
     public String toString(String left, String sep, String right) {
-        return delegate.get().toString(left,sep,right);
+        return getDelegate().toString(left,sep,right);
     }
 
     @Override
     public Iterator<T> iterator() {
-        return delegate.get().iterator();
+        return getDelegate().iterator();
+    }
+
+
+    @Override
+    public int hashCode() {
+        return getDelegate().hashCode();
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        return getDelegate().equals(obj);
+    }
+
+    @Override
+    public String toString() {
+        return getDelegate().toString();
     }
 }
