c2772eca818ddd423e9232cd22a5df8647154f67
Peter Muys <pmu@schaubroeck.be>
2016-09-28 16:15:21 +0200

    added builders NOT and SET

---
+++ src/main/java/com/persistentbit/core/utils/builders/NOT.java
@@ -0,0 +1,10 @@
+package com.persistentbit.core.utils.builders;
+
+/**
+ * Generics marker for Builder classes.<br>
+ * Used for values that are not yet set.
+ * @author Peter Muys
+ * @see com.persistentbit.core.utils.builders.SET
+ */
+public interface NOT {
+}
---
+++ src/main/java/com/persistentbit/core/utils/builders/SET.java
@@ -0,0 +1,10 @@
+package com.persistentbit.core.utils.builders;
+
+/**
+ * Generics marker for Builder classes.<br>
+ * Used for values that are set.
+ * @author Peter Muys
+ * @see com.persistentbit.core.utils.builders.NOT
+ */
+public interface SET {
+}
