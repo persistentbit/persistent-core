c41aa0fef5abaa970a6b70323f761123f8d2cf08
Peter Muys <peter@petermuys.com>
2016-10-07 19:25:52 +0200

    Added String utility functions

--- src/main/java/com/persistentbit/core/utils/StringUtils.java
+++ src/main/java/com/persistentbit/core/utils/StringUtils.java
@@ -1,6 +1,7 @@
 package com.persistentbit.core.utils;
 
 import com.persistentbit.core.NotNullable;
+import com.persistentbit.core.collections.PList;
 
 import java.util.Objects;
 
@@ -22,6 +23,7 @@ public class StringUtils {
      * </ul>
      * @param s The unescaped string (can't be null)
      * @return The escaped string
+     * @see #unEscapeJavaString(String)
      */
     static public String escapeToJavaString(String s){
         Objects.requireNonNull(s,"Can't escape a null string");
@@ -46,6 +48,50 @@ public class StringUtils {
                 sb.append(c);
             }
         }
+        return sb.toString();
+    }
+
+    /**
+     * Does the reverse of {@link #escapeToJavaString(String)}
+     * @param s The java source escaped string
+     * @return The unescaped string
+     */
+    static public  String unEscapeJavaString(String s) {
+        Objects.requireNonNull(s,"Can't unescape a null string");
+        StringBuilder sb = new StringBuilder(10);
+        boolean prevSpecial =   false;
+        for(int t=0; t<s.length();t++){
+            char c = s.charAt(t);
+            if(prevSpecial){
+                switch (c){
+                    case 't': sb.append('\t');break;
+                    case '\\': sb.append('\\');break;
+                    case 'n': sb.append('\n');break;
+                    case 'r': sb.append('\r');break;
+                    case 'b': sb.append('\b');break;
+                    case '\"': sb.append('\"');break;
+                    case '\'': sb.append('\'');break;
+                    default: sb.append('\\').append(c);break;
+                }
+                prevSpecial = false;
+            }
+            else {
+                if(c == '\\'){
+                    prevSpecial = true;
+                } else {
+                    if(prevSpecial){
+                        sb.append('\\');
+                        prevSpecial = false;
+                    }
+                    sb.append(c);
+                }
+            }
+
+        }
+        if(prevSpecial){
+            sb.append('\\');
+        }
+
         return sb.toString();
     }
 
@@ -84,4 +130,19 @@ public class StringUtils {
         }
         return txt.substring(0,txt.length()-charCount);
     }
+
+    /**
+     * Splits a string on a combination of \r\n \n or \r.
+     * @param s The String to split
+     * @return A PList of Strings without the nl or cr characters
+     */
+    static public PList<String> splitInLines(String s){
+        Objects.requireNonNull(s);
+        PList<String> res = PList.empty();
+        for(String line : s.split("\\r\\n|\\n|\\r")){
+            res = res.plus(line);
+        }
+        return res;
+    }
+
 }
