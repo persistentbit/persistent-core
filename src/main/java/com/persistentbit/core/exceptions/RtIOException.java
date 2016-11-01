e504d1faa4ad3ad57b6fd14ea27d1430d2a45391
Peter Muys <peter@petermuys.com>
2016-11-01 12:43:57 +0100

    Added DbSubstemaGen to generate Substema code from dabase table descriptions.

--- src/main/java/com/persistentbit/core/exceptions/RtIOException.java
+++ src/main/java/com/persistentbit/core/exceptions/RtIOException.java
@@ -3,7 +3,7 @@ package com.persistentbit.core.exceptions;
 import java.io.IOException;
 
 /**
- * Unchecked Mapper to be used around an IO Exception
+ * Unchecked Mapper to be used around an {@link IOException}
  *
  * @author petermuys
  * @since 28/10/16
@@ -25,14 +25,6 @@ public class RtIOException extends RuntimeException{
 	throw new RtIOException(message, cause);
   }
 
-  @FunctionalInterface
-  public interface IOExceptionCode<T>{
-		T run() throws IOException;
-  }
-  @FunctionalInterface
-  public interface IOExceptionCodeNoResult{
-	void run() throws IOException;
-  }
   public static <T> T tryRun(IOExceptionCode<T> code){
 	try{
 	  return code.run();
@@ -40,12 +32,25 @@ public class RtIOException extends RuntimeException{
 	  throw new RtIOException(io);
 	}
   }
+
   public static void tryRun(IOExceptionCodeNoResult code){
 	try{
 	  code.run();
 	}catch(IOException io){
 	  throw new RtIOException(io);
 	}
+  }
+
+	@FunctionalInterface
+	public interface IOExceptionCode<T>{
+
+		T run() throws IOException;
+	}
+
+	@FunctionalInterface
+	public interface IOExceptionCodeNoResult{
+
+		void run() throws IOException;
   }
 
 }
