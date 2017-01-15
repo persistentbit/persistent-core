eef7b6ced109b54c3884f64fd7dcc7f1d6ad5645
Peter Muys <peter@petermuys.com>
2017-01-15 11:13:53 +0100

    :construction: logging

--- src/main/java/com/persistentbit/core/sourcegen/SourcePath.java
+++ src/main/java/com/persistentbit/core/sourcegen/SourcePath.java
@@ -1,5 +1,7 @@
 package com.persistentbit.core.sourcegen;
 
+import com.persistentbit.core.result.Result;
+
 import java.net.URISyntaxException;
 import java.net.URL;
 import java.nio.file.Path;
@@ -12,29 +14,47 @@ import java.nio.file.Paths;
  */
 public final class SourcePath{
 
-  public static Path findTestSourcePath(Class<?> cls, String resourceName) {
-	return findProjectPath(cls, resourceName).resolve("src").resolve("test").resolve("java");
+  public static Result<Path> findTestSourcePath(Class<?> cls, String resourceName) {
+  	return Result.function(cls.getSimpleName(),resourceName).code(l->
+		findProjectPath(cls, resourceName)
+			.map(p -> p.resolve("src").resolve("test").resolve("java"))
+	);
+
+  }
+	public static Result<Path> findTestResourcePath(Class<?> cls, String resourceName) {
+		return Result.function(cls.getSimpleName(),resourceName).code(l->
+																		  findProjectPath(cls, resourceName)
+																			  .map(p -> p.resolve("src").resolve("test").resolve("resources"))
+		);
+
+	}
+
+  public static Result<Path> findProjectPath(Class<?> cls, String resourceName) {
+  	return Result.function(cls.getSimpleName(),resourceName).code(l -> {
+		URL url = cls.getClassLoader().getResource(resourceName);
+		if(url == null) {
+			Result.failure(new IllegalArgumentException("Can't find resource '" + resourceName + "' using classloader for " + cls
+				.getName()));
+		}
+		Path f;
+		try {
+			f = Paths.get(url.toURI());
+		} catch(URISyntaxException e) {
+			return Result.failure(e);
+		}
+		while(f.getFileName().toString().equals("target") == false) {
+			f = f.getParent();
+		}
+		return Result.success(f.getParent());
+	});
+
   }
 
-  public static Path findProjectPath(Class<?> cls, String resourceName) {
-	URL url = cls.getClassLoader().getResource(resourceName);
-	if(url == null) {
-	  throw new IllegalArgumentException("Can't find resource '" + resourceName + "' using classloader for " + cls
-																												 .getName());
-	}
-	Path f;
-	try {
-	  f = Paths.get(url.toURI());
-	} catch(URISyntaxException e) {
-	  throw new RuntimeException(e);
-	}
-	while(f.getFileName().toString().equals("target") == false) {
-	  f = f.getParent();
-	}
-	return f.getParent();
-  }
+  public static Result<Path> findSourcePath(Class<?> cls, String resourceName) {
+	return Result.function(cls.getSimpleName(),resourceName).code(l ->
+	  findProjectPath(cls, resourceName)
+		.map(p -> p.resolve("src").resolve("main").resolve("java"))
+	);
 
-  public static Path findSourcePath(Class<?> cls, String resourceName) {
-	return findProjectPath(cls, resourceName).resolve("src").resolve("main").resolve("java");
   }
 }
