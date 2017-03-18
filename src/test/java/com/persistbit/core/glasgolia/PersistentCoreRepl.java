c8b4d6602787e0d04b911546c17685575af8bf4c
Peter Muys <peter@petermuys.com>
2017-03-18 07:36:55 +0100

    persistent core repl

--- src/test/java/com/persistbit/core/glasgolia/PersistentCoreRepl.java
+++ src/test/java/com/persistbit/core/glasgolia/PersistentCoreRepl.java
@@ -1,5 +1,8 @@
 package com.persistbit.core.glasgolia;
 
+import com.persistentbit.core.glasgolia.repl.GlasgoliaRepl;
+import com.persistentbit.core.glasgolia.repl.ReplConfig;
+
 /**
  * TODOC
  *
@@ -9,6 +12,9 @@ package com.persistbit.core.glasgolia;
 public class PersistentCoreRepl {
 
 	public static void main(String[] args) throws Exception {
+		ReplConfig config = new ReplConfig().withReplInitResourceName("persistentcore.repl.test");
+		GlasgoliaRepl repl = new GlasgoliaRepl(config);
+		repl.startRepl();
 		//GGRepl repl = new GGRepl()
 		//	.loadAndEval("persistentcore.repl.test.gg");
 		//repl.repl();
